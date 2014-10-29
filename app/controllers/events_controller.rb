ea163b41e5cb659d89b61363209b7b6cd8010cd9
lbhasemeyer <lbhasemeyer@gmail.com>
2014-10-29 11:39:05 -0600

    added functionality to click on user name from index page and be lead to a new show page for that specific user.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -9,10 +9,26 @@ class EventsController < ApplicationController
   end
 
   def show
+    set_event
+  end
+
+  def edit
+    set_event
+  end
+
+  def update
+    respond_to do |format|
+      set_event
+      if @event.update(event_params)
+        format.html { redirect_to @event, notice: 'Event was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
   end
 
   def create
-    @event = Event.new(event_params)
+    @event = Event.new event_params
     respond_to do |format|
       if @event.save
         format.html { redirect_to events_path, notice: 'Event was successfully created.' }
@@ -24,6 +40,10 @@ class EventsController < ApplicationController
 
   def event_params
       params.require(:event).permit(:date, :location, :description, :capacity, :requires_id)
+  end
+
+  def set_event
+    @event = Event.find params[:id]
   end
 
 end
